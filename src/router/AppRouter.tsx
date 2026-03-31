75d512ce249b659f4d60d2123c4df19ac0f5d396
Eddie Santiago <eddiesantiago122007@gmail.com>
2026-03-31 16:34:37 -0500

    🔗 routes: Configure react router to handle with sessions

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,11 +1,21 @@
 import { Routes, Route } from "react-router-dom";
-import { Home } from "@/pages";
+import { Home, SignUp } from "@/pages";
+import { useAuth } from "@/features/auth/hooks/useAuth";
+import { PageLoader } from "@/shared/components/layout/PageLoader";
 
 export const AppRouter = () => {
+  // * Esperar a que se compruebe si hay una sesión activa en toda la app.
+  const { loading } = useAuth();
+
+  if (loading) {
+    return <PageLoader />;
+  }
+
   return (
     <Routes>
       {/* Rutas Públicas */}
       <Route path="/" element={<Home />} />
+      <Route path="/signup" element={<SignUp />} />
     </Routes>
   );
 };
