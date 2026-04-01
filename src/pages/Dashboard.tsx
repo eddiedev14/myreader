7debf5e4c7d9ae11f102ec6c3a7201d9b416b50f
Eddie Santiago <eddiesantiago122007@gmail.com>
2026-03-31 21:26:00 -0500

    ♻️ refactor: Adapt AppLayout as a route element

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,15 +1,12 @@
 import { useAuth } from "@/features/auth/hooks/useAuth";
-import { AppLayout } from "@/shared/components/layout/AppLayout";
+import { Header } from "@/shared/components/layout/Header";
 
 export const Dashboard = () => {
   const { user } = useAuth();
 
   return (
-    <AppLayout
-      title={`Bienvenido, ${user?.username || "Usuario"}!`}
-      description="Explora tus libros seleccionados, colecciones personalizadas y continúa con tus lecturas en progreso"
-    >
-      <h3>Prueba</h3>
-    </AppLayout>
+    <>
+      <Header title={`Bienvenido, ${user?.username || "Usuario"}!`} paragraph="Explora tus libros seleccionados, colecciones personalizadas y continúa con tus lecturas en progreso" />
+    </>
   );
 };
