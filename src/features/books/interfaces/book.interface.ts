0e9826d00485cd979cd4065921b2d346e4fc5c69
leiton <sebastian.leiton@uao.edu.co>
2026-04-25 21:43:30 -0500

    ♻️ Refactor: Update type problem and adecuate getBooks logic

---
+++ src/features/books/interfaces/book.interface.ts
@@ -0,0 +1,19 @@
+import type { BookGenres } from "../data/BookGenre";
+
+export interface Book {
+  // Id para firestore
+  id: string;
+
+  // Info del libro
+  title: string;
+  synopsis: string;
+  authors: string[];
+  bookCover: string | null;
+  ISBN: string;
+  mainGenre: BookGenres;
+  genres: BookGenres[];
+
+  // Info de creación
+  creatorId: string;
+  createdAt: number;
+}
