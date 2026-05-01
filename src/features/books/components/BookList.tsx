aea5547b4f54693b781be9b3a809c1af1948938f
leiton <sebastian.leiton@uao.edu.co>
2026-05-01 12:51:56 -0500

    ♻️ Refactor: Change the BookList logic to use a Double Circular Linked List and its respective navigation

--- src/features/books/components/BookList.tsx
+++ src/features/books/components/BookList.tsx
@@ -2,13 +2,13 @@ import { useState } from "react";
 import { useBook } from "../hooks/useBook";
 import { BookCard } from "./BookCard";
 import type { Book } from "../interfaces/book.interface";
+import DoubleCircularLinkedList from "../algorithms/DoubleCircularLinkedList";
+import getVisibleNodes from "../utils/visibleNodes";
 
 export function BookList() {
   const { books, getPaginatedBooks, totalPages } = useBook();
-
-  const [page, setPage] = useState(1);
-
-  const paginatedBooks = getPaginatedBooks(page);
+  const booksList = new DoubleCircularLinkedList(totalPages);
+  const [currentNode, setCurrentNode] = useState(booksList.head!);
 
   if (!books.length) {
     return (
@@ -18,24 +18,43 @@ export function BookList() {
 
   return (
     <div className="space-y-4">
-      {/* Cuadricula para los elementos */}
+      {/* Cuadrícula para los elementos */}
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-4">
-        {paginatedBooks.map((book: Book) => (
+        {getPaginatedBooks(currentNode!.page).map((book: Book) => (
           <BookCard key={book.id} book={book} />
         ))}
       </div>
 
-      {/* Paginas */}
+      {/* Botones de paginación */}
       <div className="flex justify-center gap-2">
-        {Array.from({ length: totalPages }).map((_, i) => (
+        <button
+          onClick={() => setCurrentNode(booksList.prev(currentNode!))}
+          className="px-3 py-1 border rounded"
+        >
+          <i className="ri-arrow-drop-left-line" />
+        </button>
+
+        {getVisibleNodes(currentNode!, totalPages, 6).map((node) => (
           <button
-            key={i}
-            onClick={() => setPage(i + 1)}
-            className="px-3 py-1 border rounded"
+            key={node.page}
+            onClick={() => setCurrentNode(node)}
+            className={`px-3 py-1 rounded 
+            ${
+              currentNode!.page === node.page
+                ? "bg-orange-500 text-white"
+                : "bg-white text-black border"
+            }`}
           >
-            {i + 1}
+            {node.page}
           </button>
         ))}
+
+        <button
+          onClick={() => setCurrentNode(booksList.next(currentNode!))}
+          className="px-3 py-1 border rounded"
+        >
+          <i className="ri-arrow-drop-right-line" />
+        </button>
       </div>
     </div>
   );
