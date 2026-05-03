9ba4266def84dfc3c8e776aafa95de6e55bf89a9
Eddie Santiago <eddiesantiago122007@gmail.com>
2026-05-03 10:21:14 -0500

    ♻️ refactor: Remove title key from comment interface

--- src/features/books/interfaces/comment.interface.ts
+++ src/features/books/interfaces/comment.interface.ts
@@ -1,13 +1,7 @@
 export interface Comment {
-  // Id para firestore
   id: string;
-
-  // Info del comentario
-  title: string;
   content: string;
   parentId: string | null;
-
-  // Info de creación
   creatorId: string;
   createdAt: number;
 }
