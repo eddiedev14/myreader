d26bceb0f21e6d3fc761dea822c69c612ebd4690
Eddie Santiago <eddiesantiago122007@gmail.com>
2026-05-03 10:48:19 -0500

    ✨ feat: Add helper fn to transform realtime db results to a n-ary tree

---
+++ src/features/books/utils/buildCommentTree.ts
@@ -0,0 +1,35 @@
+import CommentNode from "../algorithms/tree/CommentNode";
+import type { Comment } from "../interfaces/comment.interface";
+
+export const buildCommentTree = (comments: Comment[]): CommentNode[] => {
+  const nodeMap = new Map<string, CommentNode>();
+  const roots: CommentNode[] = [];
+
+  //* 1. Crear nodos
+  comments.forEach((comment) => {
+    nodeMap.set(comment.id, new CommentNode(comment));
+  });
+
+  //* 2. Construir jerarquía
+  comments.forEach((comment) => {
+    const node = nodeMap.get(comment.id);
+
+    if (!node) return;
+
+    //? Comentario raíz
+    if (comment.parentId === null) {
+      roots.push(node);
+      return;
+    }
+
+    //? Buscar padre
+    const parentNode = nodeMap.get(comment.parentId);
+
+    //? Si existe padre, agregar respuesta
+    if (parentNode) {
+      parentNode.addChild(node);
+    }
+  });
+
+  return roots;
+};
