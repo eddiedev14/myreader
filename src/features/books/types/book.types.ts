c9a18f8d128ac642a60c7db65769dd3e9583de55
Eddie Santiago <eddiesantiago122007@gmail.com>
2026-05-10 09:25:02 -0500

    🏷️ types: Create BookDashboard interface

--- src/features/books/types/book.types.ts
+++ src/features/books/types/book.types.ts
@@ -1,3 +1,4 @@
 import type { Book } from "../interfaces/book.interface";
 
 export type BookFormData = Omit<Book, "id" | "creatorId" | "createdAt">;
+export type BookDashboardStates = "AGENDADO" | "EN LECTURA" | "COMPLETADO";
