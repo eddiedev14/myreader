37912643f2928eb20c96ae4b441417de46dc39c6
Eddie Santiago <eddiesantiago122007@gmail.com>
2026-05-10 11:21:57 -0500

    🎨 ui: Add state visualization into BookCard component

--- src/features/books/components/BookCard.tsx
+++ src/features/books/components/BookCard.tsx
@@ -1,9 +1,29 @@
 import type { Book } from "../interfaces/book.interface";
 import type { BookDashboard } from "@/features/dashboard/interfaces/book.interface";
+import { type BookDashboardStates } from "../../dashboard/types/book.types";
 import { useNavigate } from "react-router-dom";
 
 interface BookCardProps {
   book: Book | BookDashboard;
+}
+
+const statesMessages = {
+  AGENDADO: {
+    text: "AGENDADO",
+    className: "bg-gray-500 text-white",
+  },
+  "EN LECTURA": {
+    text: "LEYENDO",
+    className: "bg-blue-500 text-white",
+  },
+  COMPLETADO: {
+    text: "COMPLETADO",
+    className: "bg-green-500 text-white",
+  },
+};
+
+function hasState(book: Book | BookDashboard): book is BookDashboard {
+  return "status" in book;
 }
 
 export function BookCard({ book }: BookCardProps) {
@@ -11,9 +31,19 @@ export function BookCard({ book }: BookCardProps) {
 
   return (
     <div
-      className="cursor-pointer rounded-lg  p-4 shadow-sm hover:shadow-md transition"
+      className="cursor-pointer rounded-lg  p-4 shadow-sm hover:shadow-md transition relative"
       onClick={() => navigate(`/library/${book.id}`)}
     >
+      {hasState(book) && (
+        <div
+          className={`absolute top-6 right-6 px-2 py-1 text-xs font-semibold rounded ${
+            statesMessages[book.status as BookDashboardStates].className
+          }`}
+        >
+          {statesMessages[book.status as BookDashboardStates].text}
+        </div>
+      )}
+
       <img
         src={book.bookCover}
         alt={book.title}
