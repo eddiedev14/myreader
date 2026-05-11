387da15a296387d3422a412df7a3942be2d158ea
Eddie Santiago <eddiesantiago122007@gmail.com>
2026-05-11 09:55:12 -0500

    ✨ feat: Implement a pomodoro in the reading queue

--- src/pages/ReadingQueue.tsx
+++ src/pages/ReadingQueue.tsx
@@ -1,6 +1,7 @@
 import { useReadingQueue } from "@/features/dashboard/hooks/useReadingQueue";
 import { Header } from "@/shared/components/ui/sections/Header";
 import { QueueItem } from "@/features/dashboard/components/QueueItem";
+import { Pomodoro } from "@/features/dashboard/components/Pomodoro";
 
 export const ReadingQueue = () => {
   const { queue } = useReadingQueue();
@@ -19,12 +20,15 @@ export const ReadingQueue = () => {
       )}
 
       {queue.items.length > 0 && (
-        <div className="grid grid-cols-[1fr_400px] gap-4">
+        <div className="grid grid-cols-[1fr_auto] gap-4">
           <main className="w-full flex flex-col gap-6">
             {queue.items.map((book) => (
               <QueueItem key={book.id} book={book} />
             ))}
           </main>
+          <aside>
+            <Pomodoro />
+          </aside>
         </div>
       )}
     </>
