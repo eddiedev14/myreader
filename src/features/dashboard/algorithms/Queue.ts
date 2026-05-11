3042bc393917ed2942b1eb108c6e954503ae7d90
Eddie Santiago <eddiesantiago122007@gmail.com>
2026-05-10 20:47:08 -0500

    ✨ feat: Delete any book from the queue

--- src/features/dashboard/algorithms/Queue.ts
+++ src/features/dashboard/algorithms/Queue.ts
@@ -15,6 +15,14 @@ export default class Queue {
     return this.items.length > 0 ? this.items.shift() : null;
   }
 
+  removeAt(position: number) {
+    if (position < 0 || position >= this.items.length) {
+      return null;
+    }
+
+    return this.items.splice(position, 1)[0];
+  }
+
   size() {
     return this.items.length;
   }
---
+++ src/features/dashboard/hooks/useQueueItem.ts
@@ -0,0 +1,35 @@
+import type { BookDashboard } from "../interfaces/book.interface";
+import { useReadingQueue } from "./useReadingQueue";
+import Swal from "sweetalert2";
+import { toast } from "react-toastify";
+
+export const useQueueItem = (book: BookDashboard) => {
+  //* Custom hooks
+  const { removeFromQueue } = useReadingQueue();
+
+  //* Handlers
+  const handleRemoveFromQueue = () => {
+    Swal.fire({
+      title: "¿Deseas eliminar este libro de la cola de lectura?",
+      text: "Este libro será eliminado de la cola de lectura, ¿Deseas continuar?",
+      showCancelButton: true,
+      confirmButtonText: "Eliminar",
+      confirmButtonColor: "#E52F1E",
+      cancelButtonText: `Cancelar`,
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        const error = await removeFromQueue(book);
+        if (error) {
+          toast.error(error);
+          return;
+        }
+
+        toast.success("Libro eliminado de la cola de lectura");
+      }
+    });
+  };
+
+  return {
+    handleRemoveFromQueue,
+  };
+};
