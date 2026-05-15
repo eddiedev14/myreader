f26e90c6a543221face36fb83f74e3701a4e1365
leiton <sebastian.leiton@uao.edu.co>
2026-05-15 03:58:16 -0500

    ✨ Feat: Add the components into the MyCollections page

--- src/pages/MyCollections.tsx
+++ src/pages/MyCollections.tsx
@@ -1,12 +1,26 @@
+import { CollectionList } from "@/features/dashboard/components/collections/CollectionList";
+import { CreateCollectionDialog } from "@/features/dashboard/components/collections/CreateCollectionDialog";
+import { useCollection } from "@/features/dashboard/hooks/collections/useCollection";
 import { Header } from "@/shared/components/ui/sections/Header";
 
 export const MyCollections = () => {
+  //* Contexts
+  const { collections } = useCollection();
+
   return (
     <>
-      <Header
-        title="Explora tus colecciones"
-        paragraph="Crea colecciones personalizadas de libros y toma apuntes fácilmente"
-      />
+      <div className="flex flex-col gap-6">
+        <div className="flex items-start justify-between">
+          <Header
+            title="Explora tus colecciones"
+            paragraph="Crea colecciones personalizadas de libros y toma apuntes fácilmente"
+          />
+          <div className="mt-2">
+            <CreateCollectionDialog />
+          </div>
+        </div>
+        <CollectionList collections={collections} />
+      </div>
     </>
   );
 };
