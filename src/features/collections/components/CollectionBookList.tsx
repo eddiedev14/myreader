080208911b7d50193d3bcaae99f8432aad9a8351
leiton <sebastian.leiton@uao.edu.co>
2026-05-18 18:53:30 -0500

    ✨ Feat: Add the collection logic and components to show the Books in the collection and interact with them

--- src/features/collections/components/CollectionBookList.tsx
+++ src/features/collections/components/CollectionBookList.tsx
@@ -1,53 +1,44 @@
-/** biome-ignore-all lint/a11y/useButtonType: <explanation> */
 import { useState, useMemo, useEffect } from "react";
-import { CollectionCard } from "./CollectionCard";
-import type { Collection } from "../interfaces/collection.interface";
+import { getVisibleNodes } from "../../books/utils/utils";
+import { PAGE_SIZE, VISIBLE_PAGES } from "../../books/constants/book.constants";
+import type { Book } from "../../books/interfaces/book.interface";
+import type { BookDashboard } from "@/features/dashboard/interfaces/book.interface";
 import DoubleCircularLinkedList from "@/shared/algorithms/doubleCircularLinkedList/DoubleCircularLinkedList";
-import {
-  PAGE_SIZE,
-  VISIBLE_PAGES,
-} from "@/features/books/constants/book.constants";
-import { getVisibleNodes } from "@/features/books/utils/utils";
+import { CollectionBookCard } from "./CollectionBookCard";
 
 interface Props {
-  collections?: Collection[];
+  books?: Book[] | BookDashboard[];
 }
 
-export function CollectionList({ collections = [] }: Props) {
-  const collectionsList = useMemo(
-    () => new DoubleCircularLinkedList(collections),
-    [collections],
-  );
+export function CollectionBookList({ books = [] }: Props) {
+  const booksList = useMemo(() => new DoubleCircularLinkedList(books), [books]);
   const [currentPage, setCurrentPage] = useState(1);
 
   const currentNode = useMemo(() => {
-    return collectionsList.getNode(currentPage);
-  }, [collectionsList, currentPage]);
+    return booksList.getNode(currentPage);
+  }, [booksList, currentPage]);
 
   useEffect(() => {
     setCurrentPage(1);
-  }, []);
+  }, [books]);
 
-  if (!collections.length) {
+  if (!books.length) {
     return (
       <div className="w-full h-64 flex items-center justify-center bg-gray-100 rounded-lg">
-        <p className="text-gray-500">No hay colecciones disponibles</p>
+        <p className="text-gray-500">No hay libros disponibles</p>
       </div>
     );
   }
 
   if (!currentNode) return null;
 
-  const totalPages = Math.ceil(collections.length / PAGE_SIZE);
+  const totalPages = Math.ceil(books.length / PAGE_SIZE);
 
   return (
     <div className="space-y-4">
       <div className="flex flex-wrap gap-4">
-        {currentNode.nodes.map((collection) => (
-          <CollectionCard
-            key={collection.id}
-            collection={collection as Collection}
-          />
+        {currentNode.nodes.map((book) => (
+          <CollectionBookCard key={book.id} book={book} />
         ))}
       </div>
 
--- src/features/collections/components/CollectionCard.tsx
+++ src/features/collections/components/CollectionCard.tsx
@@ -1,13 +1,19 @@
+import { useCollectionCard } from "../hooks/useCollectionCard";
 import type { Collection } from "../interfaces/collection.interface";
 
 interface CollectionCardProps {
   collection: Collection;
 }
 
-export function CollectionCard({ collection }: CollectionCardProps) {
+export const CollectionCard = ({ collection }: CollectionCardProps) => {
+  const { handleNavigateCollection } = useCollectionCard(collection);
+
   return (
-    <div className="w-48 cursor-pointer rounded-lg  p-4 shadow-sm hover:shadow-md transition relative">
+    <div
+      className="w-48 cursor-pointer rounded-lg  p-4 shadow-sm hover:shadow-md transition relative"
+      onClick={handleNavigateCollection}
+    >
       <p className="text-lg font-semibold mb-2">{collection.title}</p>
     </div>
   );
-}
+};
