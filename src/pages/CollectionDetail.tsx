f04f378868740bcb144c5858043b2bf663d9f62b
leiton <sebastian.leiton@uao.edu.co>
2026-05-18 20:26:14 -0500

    ✨ Feat: Implement an auth validation to make visible the collections only to their owners

--- src/pages/CollectionDetail.tsx
+++ src/pages/CollectionDetail.tsx
@@ -8,11 +8,32 @@ import { Link } from "react-router-dom";
 import LibraryIllustration from "../assets/illustrations/library.svg";
 
 export const CollectionDetail = () => {
-  const { loading, collection, getCollectionBooks } = useCollectionDetail();
+  const { loading, collection, isAuthorized, getCollectionBooks } =
+    useCollectionDetail();
 
   if (loading) {
     return <PageLoader />;
   }
+
+  if (!isAuthorized) {
+    return (
+      <section className="mx-auto flex flex-col gap-4">
+        <img
+          src={LibraryIllustration}
+          alt="Library Illustration"
+          className="w-md"
+        />
+        <h2 className="text-center text-2xl font-bold">Acceso denegado</h2>
+        <p className="text-center font-light">
+          No tienes permisos para ver esta colección.
+        </p>
+        <Button asChild size="lg" variant="outline">
+          <Link to="/my-collections">Regresar a tus colecciones</Link>
+        </Button>
+      </section>
+    );
+  }
+
   if (!collection) {
     return (
       <section className="mx-auto flex flex-col gap-4">
