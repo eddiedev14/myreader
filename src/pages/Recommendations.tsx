80a6b83e005cc155c71afa4b6f77d210a0cbc095
Eddie Santiago <eddiesantiago122007@gmail.com>
2026-05-22 09:04:04 -0500

    ✨ feat: Add useRecommendations hook

--- src/pages/Recommendations.tsx
+++ src/pages/Recommendations.tsx
@@ -1,6 +1,15 @@
+import { useRecommendations } from "@/features/recommendations/hooks/useRecommendations"
 import { Header } from "@/shared/components/ui/sections/Header"
+import { PageLoader } from "@/shared/components/ui/sections/PageLoader";
 
 export const Recommendations = () => {
+    //* Custom hooks
+    const { loading } = useRecommendations();
+
+    if (loading) {
+        <PageLoader />
+    }
+
     return (
         <Header title="Recomendaciones" paragraph="En MyReader te ayudamos a descubrir nuevos libros que puedan interesarte" />
     )
