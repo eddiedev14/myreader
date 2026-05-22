da0e7b8e9e350055a4babb4a77413514f1957bb8
Eddie Santiago <eddiesantiago122007@gmail.com>
2026-05-22 08:52:21 -0500

    🎨 ui: Create basic recommendations page

---
+++ src/pages/Recommendations.tsx
@@ -0,0 +1,7 @@
+import { Header } from "@/shared/components/ui/sections/Header"
+
+export const Recommendations = () => {
+    return (
+        <Header title="Recomendaciones" paragraph="En MyReader te ayudamos a descubrir nuevos libros que puedan interesarte" />
+    )
+}
