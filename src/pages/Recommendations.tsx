828228feab3b180dd25b3002d9f43f2b9679e650
leiton <sebastian.leiton@uao.edu.co>
2026-05-22 10:59:01 -0500

    ✨ Feat: Implement the whole logic into the recommendation page

--- src/pages/Recommendations.tsx
+++ src/pages/Recommendations.tsx
@@ -1,16 +1,46 @@
-import { useRecommendations } from "@/features/recommendations/hooks/useRecommendations"
-import { Header } from "@/shared/components/ui/sections/Header"
+import { useRecommendations } from "@/features/recommendations/hooks/useRecommendations";
+import { Header } from "@/shared/components/ui/sections/Header";
 import { PageLoader } from "@/shared/components/ui/sections/PageLoader";
+import { RecommendationsSection } from "@/features/recommendations/components/RecommendationSection";
 
 export const Recommendations = () => {
-    //* Custom hooks
-    const { loading } = useRecommendations();
+  //* Custom hooks
+  const { authorRecommendations, genreRecommendations, loading } =
+    useRecommendations();
 
-    if (loading) {
-        <PageLoader />
-    }
+  if (loading) {
+    <PageLoader />;
+  }
 
-    return (
-        <Header title="Recomendaciones" paragraph="En MyReader te ayudamos a descubrir nuevos libros que puedan interesarte" />
-    )
-}
+  return (
+    <>
+      <Header
+        title="Recomendaciones"
+        paragraph="En MyReader te ayudamos a descubrir nuevos libros que puedan interesarte"
+      />
+      <RecommendationsSection
+        title="Recomendados por Autor"
+        description="Libros relacionados con autores que ya lees"
+        icon="ri-quill-pen-fill"
+        iconClassName="text-violet-500"
+        books={authorRecommendations}
+        recommendationReason={{
+          text: "Autor relacionado",
+          type: "author",
+        }}
+      />
+
+      <RecommendationsSection
+        title="Recomendados por Género"
+        description="Descubre lecturas similares a tus géneros favoritos"
+        icon="ri-bookmark-fill"
+        iconClassName="text-orange-500"
+        books={genreRecommendations}
+        recommendationReason={{
+          text: "Género relacionado",
+          type: "genre",
+        }}
+      />
+    </>
+  );
+};
